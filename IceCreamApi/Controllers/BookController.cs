dc200a1f1224403bf223f2d4d40c97cab5a78577
S.Ngoc <49833175+SNgoc@users.noreply.github.com>
2021-12-04 20:25:06 +0700

    bok04122021

---
+++ IceCreamApi/Controllers/BookController.cs
@@ -0,0 +1,88 @@
+﻿using IceCreamApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        projectContext ctx;
+
+        public BookController(projectContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        //VIEW ALL BOOK
+        [HttpGet] //fix lỗi swagger
+        public async Task<ActionResult<List<BookIceCream>>> GetBooks()
+        {
+            var result = await ctx.BookIceCreams.ToListAsync();
+            if (result == null)
+            {
+                return NotFound();//mã 404
+            }
+            return Ok(result);//tìm thấy mã 200->299, mã 300 redirect
+        }
+        //END VIEW
+
+        //CREATE
+        //post create
+        [HttpPost] //fix lỗi swagger
+        public async Task<ActionResult<BookIceCream>> CreateBook(BookIceCream book)//đặt post ở đầu để đảm bảo post và fix lỗi swagger
+        {
+            ctx.BookIceCreams.Add(book);
+            await ctx.SaveChangesAsync();
+
+            //tìm employee vừa tạo
+            var b = await ctx.BookIceCreams.OrderByDescending(c => c.BookId).SingleOrDefaultAsync();
+            return CreatedAtAction("GetBooks", new { BookID = b.BookId }, b);
+        }
+        //END CREATE
+
+        //update Book
+        //lấy id theo EmpId dùng cho hàm update
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookIceCream>> GetBook(int id)
+        {
+            var result = await ctx.BookIceCreams.SingleOrDefaultAsync(c => c.BookId == id);
+            if (result == null)
+            {
+                return NotFound(); //mã 404
+            }
+            return Ok(result); //tìm thấy mã 200->299, mã 300 redirect
+        }
+
+        [HttpPut]//vì dùng method Put khác method update salary nên ko sợ trùng route
+        public async Task<ActionResult> PutUpdateBook(BookIceCream book)//đặt put ở đầu để đảm bảo put
+        {
+            ctx.Entry(book).State = EntityState.Modified;
+            await ctx.SaveChangesAsync();
+
+            return NoContent();
+        }
+        //end update
+
+        //delete Book
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var result = await ctx.BookIceCreams.SingleOrDefaultAsync(c => c.BookId == id);
+            if (result == null)
+            {
+                return NotFound(); //mã 404
+            }
+            ctx.BookIceCreams.Remove(result); //delete
+            await ctx.SaveChangesAsync();
+
+            return NoContent(); //trả về mã thành công
+        }
+    }
+}
