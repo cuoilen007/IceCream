3442056066075545636dd5f179b8107d1e554011
cuoilen007 <havy59@gmail.com>
2021-12-14 14:30:15 +0700

    adsadsa

--- IceCreamClient/Models/BookOrder.cs
+++ IceCreamClient/Models/BookOrder.cs
@@ -1,4 +1,6 @@
-﻿namespace IceCreamClient.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IceCreamClient.Models
 {
     public class BookOrder
     {
@@ -7,6 +9,9 @@
         public string Address { get; set; }
         public string Fullname { get; set; }
         public int? TotalPrice { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]{1,50}$",
+        ErrorMessage = "Please Just Input Number")]
         public string Phone { get; set; }
 
         public bool? IsCompleted { get; set; }
